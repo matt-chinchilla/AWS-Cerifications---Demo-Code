bbc1dfc81bf1f604a386423e0f5133cd903cf56a
matt-chinchilla <70705923+matt-chinchilla@users.noreply.github.com>
2024-10-25 16:09:51 -0400

    Update dependencies

--- 93. Lab Session 1a - Programming SQS and SNS with NodeJS/index.js
+++ 93. Lab Session 1a - Programming SQS and SNS with NodeJS/index.js
@@ -15,8 +15,8 @@ var queueUrl;
 var params = {
     QueueName: 'chirichella-lab',
     Attributes: {
-        RecieveMessageWaitTimeSeconds: '20',
-        VisbilityTimeout: '60'
+        ReceiveMessageWaitTimeSeconds: '20',
+        VisibilityTimeout: '60'
     }
 };
 sqs.createQueue(params, function(err, data) { //createQueue function takes two parameters: params and a callback function
@@ -24,5 +24,28 @@ sqs.createQueue(params, function(err, data) { //createQueue function takes two p
         console.log(err, err.stack);
     else{
         console.log('Successfully created SQS queue URL ' + data.QueueUrl); // successful response
+        createMessages(data.QueueUrl);
     }
 });
+
+// Making the createMessages function
+async function createMessages(queueUrl) {
+    var messages = [];
+    for (var a=0; a<50; a++){ 
+        messages[a] = 'This is the content for message ' + a + '.';
+    }
+    // Asynchronously send messages to the SQS queue
+    for(const message of messages){ // for of loop
+        // console.log(message) // Prints each message
+        var params;
+        console.log('Sending message ' + message)
+        params = {
+            MessageBody: message, /*required*/
+            QueueUrl: queueUrl
+        };
+        await sqs.sendMessage(params, function(err, data) { // Wait until callback
+        if (err) console.log(err, err.stack); // an error occurred
+        else     console.log(data);           // successful response
+        });
+    }
+}
