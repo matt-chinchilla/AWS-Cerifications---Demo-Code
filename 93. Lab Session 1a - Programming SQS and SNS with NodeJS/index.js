9099d535c5d36966e1188572f62a8ed2d86f96e9
matt-chinchilla <70705923+matt-chinchilla@users.noreply.github.com>
2024-10-25 16:42:04 -0400

    Refactor code to send SQS messages in batches

--- 93. Lab Session 1a - Programming SQS and SNS with NodeJS/index.js
+++ 93. Lab Session 1a - Programming SQS and SNS with NodeJS/index.js
@@ -24,28 +24,61 @@ sqs.createQueue(params, function(err, data) { //createQueue function takes two p
         console.log(err, err.stack);
     else{
         console.log('Successfully created SQS queue URL ' + data.QueueUrl); // successful response
-        createMessages(data.QueueUrl);
+        // Save queue url
+        queueUrl = data.QueueUrl;
+        // waitingSQS to false
+        waitingSQS = false;
+        createMessages(data.QueueUrl); // Creates content for the message body
     }
 });
 
-// Making the createMessages function
-async function createMessages(queueUrl) {
+// Create 50 SQS messages
+async function createMessages(queueUrl){
     var messages = [];
-    for (var a=0; a<50; a++){ 
-        messages[a] = 'This is the content for message ' + a + '.';
+    for (var a=0; a<5; a++){
+        messages[a] = [];
+        for (var b=0; b<10; b++){
+            messages[a][b] = 'This is the content for message '+ (a*10+b) + '.';
+        }
     }
-    // Asynchronously send messages to the SQS queue
-    for(const message of messages){ // for of loop
-        // console.log(message) // Prints each message
-        var params;
-        console.log('Sending message ' + message)
+    // Asynchronously deliver messages to SQS queue
+    for (const message of messages){
+        console.log('Sending message: '+ message)
         params = {
-            MessageBody: message, /*required*/
-            QueueUrl: queueUrl
+            Entries: [],
+            QueueUrl: queueUrl /* required */
         };
-        await sqs.sendMessage(params, function(err, data) { // Wait until callback
+    for (var b=0; b<10; b++){
+        params.Entries.push({
+            MessageBody: message [b],
+            Id: 'Message'+ (messages.indexOf(message)*10+b)
+            });
+        }
+    await sqs.sendMessageBatch(params, function(err, data) { // Wait until callback
         if (err) console.log(err, err.stack); // an error occurred
-        else     console.log(data);           // successful response
-        });
+        else console.log(data); // successful response
+         });
+     }
     }
-}
+
+// // Making the createMessages function
+// async function createMessages(queueUrl) {
+//     var messages = [];
+//     for (var a=0; a<50; a++){ 
+//         messages[a] = 'This is the content for message ' + a + '.';
+//     }
+//     // Asynchronously send messages to the SQS queue
+//     for(const message of messages){ // for-of loop
+//         // console.log(message) // Prints each message
+//         var params;
+//         console.log('Sending message ' + message)
+//         params = {
+//             MessageBody: message, /*required*/
+//             QueueUrl: queueUrl
+//         };
+//         await sqs.sendMessage(params, function(err, data) { // Wait until callback
+//         if (err) console.log(err, err.stack); // an error occurred
+//         else     console.log(data);           // successful response
+//         });
+//     }
+// }
