3d6ccf7727627ee761c2605bf07ded2c998c1acb
matt-chinchilla <70705923+matt-chinchilla@users.noreply.github.com>
2024-10-28 14:44:30 -0400

    Refactor code to send SQS messages in batches and add message processing functionality

--- 93. Lab Session 1a - Programming SQS and SNS with NodeJS/index.js
+++ 93. Lab Session 1a - Programming SQS and SNS with NodeJS/index.js
@@ -32,6 +32,21 @@ sqs.createQueue(params, function(err, data) { //createQueue function takes two p
     }
 });
 
+// Make a timer that can poll the SQS queue every 60 seconds
+var waitingSQS = true;
+var queueCounter = 0;
+
+setInterval(function(){
+    if (!waitingSQS){ // Still busy with previous request
+        if (queueCounter <= 0){
+            receiveMessages();
+        }
+        else{
+            --queueCounter; // Reduce the counter
+        }
+    }
+}, 1000); // Every 1000ms or 1s
+
 // Create 50 SQS messages
 async function createMessages(queueUrl){
     var messages = [];
@@ -60,6 +75,56 @@ async function createMessages(queueUrl){
          });
      }
     }
+
+// Make the receive messages function
+function receiveMessages(){
+    var params = {
+        QueueUrl: queueUrl, /* required */
+        MaxNumberOfMessages: 10,
+        VisibilityTimeout: 60,
+        WaitTimeSeconds: 20 // Wait time (in seconds) for messages to arrive
+    };
+    waitingSQS = true;
+    sqs.receiveMessage(params, function(err, data) {
+        if (err) {
+            waitingSQS = false;
+            console.log(err, err.stack); // an error occurred, console.log it out
+        }
+        else{
+            if((typeof data.Messages !== 'undefined') && (data.Messages.length !== 0)/* if messages Array is not equal to 0 */) {
+                console.log('Received ' + data.Messages.length // Tells me # of messages received
+                    + ' messages from SQS queue.');
+                processMessages(data.Messages);
+            }
+            else{
+                queueCounter = 60; // Wait a min to call function again
+                console.log('SQS queue empty, waiting for ' + queueCounter + 's.');
+            }
+        }
+    });
+}
+
+// Make the processMessages function to process & delete messages from the queue
+async function processMessages(messagesSQS) {
+    for (const item of messagesSQS) {
+        await console.log('Processing message: ' + item.Body); // Do something with the message
+        var params = {
+            QueueUrl: queueUrl, /* required */
+            ReceiptHandle: item.ReceiptHandle /* required */
+        };
+        await sqs.deleteMessage(params, function(err, data) { // Wait until callback
+            if (err) {
+                console.log(err, err.stack); // an error occurred
+            } else {
+                if (data && data.ResponseMetadata) {
+                    console.log('Deleted message RequestId: ' + JSON.stringify(data.ResponseMetadata.RequestId)); // successful response
+                } else {
+                    console.log('Deleted message but no RequestId returned');
+                }
+            }
+        });
+    }
+}
 
 // // Making the createMessages function
 // async function createMessages(queueUrl) {
